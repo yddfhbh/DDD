72e8584b175ccfb5189f49ffeda0feb2c67b7e5b
yddfhbh <yddfhbh@gmail.com>
2026-05-29 17:50:24 +0900

    Add GuildMembers and GuildPresences intents

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences
   ],
 });
 
